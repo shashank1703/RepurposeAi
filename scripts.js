942e55fff84fe1fe7c00aba7626bdd6d90b300cd
shashank1703 <shashankbhargava07@gmail.com>
2024-05-16 20:36:52 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function navigateTo(page) {
+    // Simulating navigation for this example
+    alert(`Navigating to ${page} page...`);
+    // In a real application, you would use something like:
+    window.location.href = `${page}.html`;
+}
